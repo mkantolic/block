0db33fc51bad034cf40a16cd3ec9f127d64cb554
Marija <mkantolic@unipu.hr>
2021-09-05 10:23:35 +0200

    second commit

---
+++ contracts/Greeter.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Greeter {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting:", _greeting);
+        greeting = _greeting;
+    }
+
+    //funkcija koja omogućava čitanje s blockchaina
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    //funkcija za updateanje greeting value s argumentom 
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
+}
